4aaaa0519662f761124cb82538fd06d3ce4277bc
Wills Bithrey <wills@seventysevenpixels.com>
2012-10-12 20:00:17 +0100

    Adding screen clearing

--- js/main.js
+++ js/main.js
@@ -3,13 +3,19 @@ var canvas = document.getElementById('PartCanvas');
 var ctx = canvas.getContext('2d');
 var particles = [];
 
+// Set the canvas dimensions
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
-ctx.fillStyle="#000000";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-ctx.save();
-ctx.translate(canvas.width / 2, canvas.height / 2);
 
+// Fill the screen with black
+ctx.fillStyle = "#000000";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+// Save the canvas state as this is our initial starting point
+ctx.save();
+
+// Move the canvas origin to the centre of the screen
+ctx.translate(canvas.width / 2, canvas.height / 2);
 
 function init(){
     for(i=0; i<20; i++){
@@ -20,6 +26,11 @@ function init(){
 }
 
 function draw(){
+    // Clear the screen
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(-canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
+
+    // Draw particles
     ctx.fillStyle="#FF0000";
     for(i=0; i<particles.length; i++){
         var p = particles[i];
